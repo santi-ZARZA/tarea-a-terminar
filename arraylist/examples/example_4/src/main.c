5996795b4a63a00286cceeafc055f03c60e2cd41
santizarza <santi-zarza@hotmail.com>
2017-11-05 17:02:17 -0300

    Signed-off-by: santizarza <santi-zarza@hotmail.com>

---
+++ arraylist/examples/example_4/src/main.c
@@ -0,0 +1,72 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2016>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/ArrayList.h"
+#include "../inc/Employee.h"
+
+//        startTesting(1);--->  //RESUELTA AL_NEWARRAYLIST
+//        startTesting(2);--->  //RESUELTA AL_ADD
+//        startTesting(3);--->  //RESUELTA AL_DELETEARRAYLIST
+//        startTesting(4);--->  //RESUELTA AL_LEN
+//        startTesting(5);--->  //RESUELTA AL_GET (  PREGUNTAR POR NOTACION DE PUNTEROS  )
+//        startTesting(6);--->  //RESUELTA AL_CONTAINS (  PREGUNTAR POR NOTACION DE PUNTEROS  )
+//        startTesting(7);--->  //RESUELTA AL_SET (  PREGUNTAR POR NOTACION DE PUNTEROS  )
+//        startTesting(8);--->  //RESUELTA AL_REMOVE (  PREGUNTAR POR NOTACION DE PUNTEROS  )
+//        startTesting(9);--->  // AL_CLEAR
+//        startTesting(10);--->  //RESUELTA AL_CLONE
+//        startTesting(11);--->  // AL_PUSH
+//        startTesting(12);--->  //RESUELTA AL_INDEXOF
+//        startTesting(13);--->  //RESUELTA AL_ISEMPTY
+//        startTesting(14);--->  // AL_POP
+//        startTesting(15);--->  // AL_SUBLIST
+//        startTesting(16);--->  // AL_CONTAINSALL
+//        startTesting(17);--->  // AL_SORT
+
+
+int main(void)
+{
+        startTesting(1);
+        startTesting(2);
+        startTesting(3);
+        startTesting(4);
+        startTesting(5);
+        startTesting(6);
+        startTesting(7);
+        startTesting(8);
+//        startTesting(9);
+        startTesting(10);
+        startTesting(11);
+        startTesting(12);
+        startTesting(13);
+        startTesting(14);
+        startTesting(15);
+//        startTesting(16);
+//        startTesting(17);
+
+    return 0;
+}
+
+
+
+
+
